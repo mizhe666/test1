85af94cfd92c10640bfadeede5307f59902661c2
mizhe666 <162081156+mizhe666@users.noreply.github.com>
2024-03-10 05:33:58 +1030

    Update question12.sh

--- question12.sh
+++ question12.sh
@@ -1,16 +1,9 @@
 # Switch back to branch2
 git checkout branch2
 
-# Manually apply changes from the backup file (file4_backup.txt assumed)
-patch -p1 < file4_backup.txt
-
-# Add the modified file4 to the staging area
-git add file4
+# Restore uncommitted changes
+git restore file4
 
 # Commit the changes
-git commit -m "Restored and committed changes in file4"
-
-# Test and verify your commands in a blank/test repository
-# ... Perform the above steps in a test repository ...
-
-# Save the commands in a file named question12.sh
+git add file4
+git commit -m "Restore and commit changes in branch2"
