c352619c1d6606aa52da5c9f44fdb32f49d199a6
mizhe666 <162081156+mizhe666@users.noreply.github.com>
2024-03-10 04:56:13 +1030

    Create question17.sh

---
+++ question17.sh
@@ -0,0 +1,23 @@
+# Change into the repository directory
+cd git-practice-03
+
+# Create branch1 and add, move, remove & modify files
+git checkout -b branch1
+mkdir dir1
+echo "foo" > dir1/foo
+mv dir3/bar_copy newfile1
+rm -r dir2
+echo "bar" > dir3/bar
+git add .
+git commit -m "Commit changes to branch1"
+
+# Create branch2 and add, move, remove & modify files
+git checkout -b branch2
+mv dir1/foo dir1/dir2/foo_modified
+echo "newfile2" > dir1/dir3/newfile2
+git rm newfile1
+git add .
+git commit -m "Commit changes to branch2"
+
+# Switch back to main
+git checkout main
