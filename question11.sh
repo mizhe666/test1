1c086030db9982d89fe556187dc13492486bba40
mizhe666 <162081156+mizhe666@users.noreply.github.com>
2024-03-10 05:21:06 +1030

    Update question11.sh

--- question11.sh
+++ question11.sh
@@ -1,15 +1,13 @@
 # Create and switch to a new branch named branch2
 git checkout -b branch2
 
-# Create and commit a file file4
+# Create, and commit a file file4
 touch file4
 git add file4
-git commit -m "Add file4"
+git commit -m "Added file4"
 
 # Modify file4
-echo "Some changes in file4" > file4
-git add file4
-git commit -m "Modify file4"
+# ... Make changes to file4 using a text editor ...
 
-# Switch back to the main branch without committing changes
+# Switch back to the main branch without committing your changes
 git checkout main
