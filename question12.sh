98095695bd2dc0af9d600a5e7c23dc02a93afa35
mizhe666 <162081156+mizhe666@users.noreply.github.com>
2024-03-10 05:03:19 +1030

    Update question12.sh

--- question12.sh
+++ question12.sh
@@ -1,6 +1,14 @@
 # Switch back to branch2
 git checkout branch2
 
-# Restore and commit uncommitted changes
-git restore --source=branch2 --staged --worktree -- file4
-git commit -m "Restore and commit changes to file4"
+# Check the status to identify uncommitted changes
+git status
+
+# If there are uncommitted changes, restore them
+# Use appropriate commands based on the nature of the changes
+# For example, if there are changes in file4.txt
+git restore file4.txt
+
+# Commit the changes
+git add .  # Add all changes
+git commit -m "Restore and commit uncommitted changes"
