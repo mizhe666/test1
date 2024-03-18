8bf0ccefbf69e204b5dd75a890c5578161f77408
mizhe666 <162081156+mizhe666@users.noreply.github.com>
2024-03-18 14:54:30 +1030

    Update question16.sh

--- question16.sh
+++ question16.sh
@@ -1,4 +1,9 @@
 git checkout branch2
+cd dir1/file2
+echo "Integer quis justo eget eros pretium facilisis quis vitae enim. Pellentesque fringilla placerat maximus. Sed at facilisis erat, sed vulputate arcu. Donec in lorem ut orci facilisis condimentum id eget libero. Ut interdum nibh sed velit commodo, ac eleifend tellus pulvinar. Maecenas bibendum eros et posuere sollicitudin. Cras venenatis aliquam risus, nec faucibus risus. Curabitur vel blandit lacus. Curabitur eu est vitae nisl placerat sodales vitae ac diam. Nunc augue dolor, malesuada nec nisi in, convallis congue nisl.
+git add file2
+git comit -m "merge"
+git push branch2
 git merge branch3
 # Resolve any merge conflicts if necessary
 # Once conflicts are resolved, continue with the following commands:
