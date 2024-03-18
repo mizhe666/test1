3bf126aebe03faa4a866666b1bd06d4845819c51
mizhe666 <162081156+mizhe666@users.noreply.github.com>
2024-03-18 14:37:41 +1030

    Update question16.sh

--- question16.sh
+++ question16.sh
@@ -1,16 +1,5 @@
-# Change into the repository directory
-cd git-practice-02
-
-# Fetch branch3 from the remote
-git fetch origin branch3
-
-# Create and switch to a new branch for the merge
-git checkout -b merge-branch3
-
-# Merge branch3 into branch2
-git merge origin/branch3
-
-# If there are merge conflicts, resolve them manually and then commit the changes
-
-# Delete branch3
+git checkout branch2
+git merge branch3
+# Resolve any merge conflicts if necessary
+# Once conflicts are resolved, continue with the following commands:
 git branch -d branch3
