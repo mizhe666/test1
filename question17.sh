ff8121ac2eba54f20f4964d1eebd5ea003ba3fdd
mizhe666 <162081156+mizhe666@users.noreply.github.com>
2024-03-18 23:15:30 +1030

    Update question17.sh

--- question17.sh
+++ question17.sh
@@ -6,11 +6,18 @@ git add .
 git commit -m "bar_copy"
 # Create branch1 and add, move, remove & modify files
 git checkout -b branch1
-cd dir1/dir2
-git rm foo
+# Remove files not needed in branch1
+git rm dir3/bar_copy
+git rm dir1/dir2
+git commit -m "Remove unnecessary files for branch1"
+
+# Move and modify files in branch1
+mv dir3/bar bar
+git mv dir1/foo dir1/
+touch newfile1
+echo "Content for newfile1" > newfile1
 git add .
-git commit -m "branch1"
-# Create branch2 and add, move, remove & modify files
+git commit -m "Move and modify files for branch1"
 git checkout -b branch2
 mv dir1/foo dir1/dir2/foo_modified
 echo "newfile2" > dir1/dir3/newfile2
